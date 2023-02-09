5e35c2155889b394a83fd530cf0925d4b274ce30
MUHAMMAD RIDOWAN AHAD <muhammadridowanahad@MUHAMMADs-MacBook-Air.local>
2023-02-09 12:03:34 +0600

    let it dry

--- script.js
+++ script.js
@@ -1,81 +1,72 @@
 "use strict";
 
-
 // * create a click event method and create conditions with if/else statement.
 
-let number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 10) + 1;
 
-let score = 20;
+let score = 10;
+
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector(".score").textContent = score;
+};
+
+const displayNumber = function (number) {
+  document.querySelector(".number").textContent = number;
+};
+
+const displayNumberWidth = function (numberWidth) {
+  document.querySelector(".number").style.width = numberWidth;
+};
+
+const displayBackgroundColor = function (bbackgroundColor) {
+  document.querySelector("body").style.backgroundColor = bbackgroundColor;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
 
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️ No entry!";
-  }
-  // when player wins the game
-  else if (guess == number) {
-    document.querySelector(".message").textContent =
-      "🎉 you guessed the correct number!";
-    document.querySelector(".number").textContent = number;
-
-    document.querySelector("body").style.backgroundColor = "#71866b";
-    document.querySelector(".number").style.width = "30rem";
-
+    displayMessage("⛔️ No Entry");
+  } else if (guess == number) {
+    displayMessage("🎉 Hurrah! you guessed the correct number");
+    displayNumber(number);
+    displayNumberWidth("30rem");
+    displayBackgroundColor("#71866b");
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
-  }
-  // when guess is to high
-  else if (guess > number) {
+  } else if (guess !== number) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too high!";
+      displayMessage(guess > number ? "📈 Too high!" : "📉 To low!");
       score--;
-      document.querySelector(".score").textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector(".message").textContent = "🥶 You lost the game";
-      document.querySelector(".score").textContent = 0;
-    }
-  }
-  // when guess is too low
-  else if (guess < number) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "🥶 You lost the game";
-      document.querySelector(".score").textContent = 0;
+      displayScore(0);
+      displayMessage("🥶 You lost the game!");
     }
   }
 });
 
-// Again playing button
-
-// when clicking again the highscore will remain
 document.querySelector(".again").addEventListener("click", function () {
-  score = 20;
-  number = Math.trunc(Math.random() * 20) + 1;
-
-  document.querySelector(".number").style.width = "15rem";
-
-  document.querySelector(".number").textContent = "?";
-
-  document.querySelector("body").style.backgroundColor = "#222";
-
-  document.querySelector(".message").textContent = "Start guessing...";
-
-  document.querySelector(".score").textContent = score;
   document.querySelector(".guess").value = "";
+  displayMessage("🤔 Start guessing...");
+
+  number = Math.trunc(Math.random() * 10) + 1;
+
+  displayNumber("?");
+  displayNumberWidth("15rem");
+  displayBackgroundColor("#222");
+
+  score = 10;
+  displayScore(score);
 });
-
-
-
-// Hope this helps you to understand the code.
-// * when checking the (".check") and the ('.guess') class, change the (".message"), (".score"),('.highscore') classes and also when the condition of ('.number') = ('.guess') then the result will be correct and the background will change.
-
 
 //others things to add
 // can edit for a mid range value condition where the message can be: you are close for the win
